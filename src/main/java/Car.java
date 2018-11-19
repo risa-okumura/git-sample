2be4a2676e7add28d194ecbc7d3a62f31635494b
okumura <risa.okumura@rakus.co.jp>
2018-11-19 10:10:39 +0900

    carクラス作成

---
+++ src/main/java/Car.java
@@ -0,0 +1,20 @@
+
+public class Car {
+	
+	private Integer speed;
+	private String color;
+	public Integer getSpeed() {
+		return speed;
+	}
+	public void setSpeed(Integer speed) {
+		this.speed = speed;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	
+
+}
